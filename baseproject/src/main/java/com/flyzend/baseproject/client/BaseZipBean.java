056233213456cc3eb025a156e398235c39e005c9
Flyzend <306217025@qq.com>
2017-08-05 11:49:55 +0800

    修改zipBean名称

--- baseproject/src/main/java/com/flyzend/baseproject/client/BaseZipBean.java
+++ baseproject/src/main/java/com/flyzend/baseproject/client/BaseZipBean.java
@@ -4,32 +4,32 @@ package com.flyzend.baseproject.client;
  * Created by 王灿 on 2017/7/24.
  */
 
-public class BaseCommentZipBean {
+public class BaseZipBean {
     private String mZipBean1;
     private String mZipBean2;
     private String mZipBean3;
     private String mZipBean4;
     private String mZipBean5;
 
-    public BaseCommentZipBean(String zipBean1, String zipBean2) {
+    public BaseZipBean(String zipBean1, String zipBean2) {
         mZipBean1 = zipBean1;
         mZipBean2 = zipBean2;
     }
 
-    public BaseCommentZipBean(String zipBean1, String zipBean2, String zipBean3) {
+    public BaseZipBean(String zipBean1, String zipBean2, String zipBean3) {
         mZipBean1 = zipBean1;
         mZipBean2 = zipBean2;
         mZipBean3 = zipBean3;
     }
 
-    public BaseCommentZipBean(String zipBean1, String zipBean2, String zipBean3, String zipBean4) {
+    public BaseZipBean(String zipBean1, String zipBean2, String zipBean3, String zipBean4) {
         mZipBean1 = zipBean1;
         mZipBean2 = zipBean2;
         mZipBean3 = zipBean3;
         mZipBean4 = zipBean4;
     }
 
-    public BaseCommentZipBean(String zipBean1, String zipBean2, String zipBean3, String zipBean4, String zipBean5) {
+    public BaseZipBean(String zipBean1, String zipBean2, String zipBean3, String zipBean4, String zipBean5) {
         mZipBean1 = zipBean1;
         mZipBean2 = zipBean2;
         mZipBean3 = zipBean3;
--- baseproject/src/main/java/com/flyzend/baseproject/client/ZipSubscriber.java
+++ baseproject/src/main/java/com/flyzend/baseproject/client/ZipSubscriber.java
@@ -6,7 +6,7 @@ import android.content.Context;
  * Created by 王灿 on 2017/8/5.
  */
 
-public abstract class ZipSubscriber extends BaseSubscriber<BaseCommentZipBean>{
+public abstract class ZipSubscriber extends BaseSubscriber<BaseZipBean>{
     public ZipSubscriber(Context context, String testTag, String loadText, boolean isShowDialog) {
         super(context, testTag,loadText, isShowDialog);
     }
