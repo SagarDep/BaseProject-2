f02e5d71ba9e65efceeeecefa305697197c41042
Flyzend <306217025@qq.com>
2017-11-15 09:24:03 +0800

    更新baseDialog

--- baseproject/src/main/java/com/flyzend/baseproject/dialog/BaseDialog.java
+++ baseproject/src/main/java/com/flyzend/baseproject/dialog/BaseDialog.java
@@ -1,28 +1,44 @@
 package com.flyzend.baseproject.dialog;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 /**
  * Created by 王灿 on 2017/2/27.
  */
 
-public class BaseDialog extends Dialog {
+public class BaseDialog {
     private Context mContext;
 
     public BaseDialog(Context context) {
-        super(context);
         mContext = context;
     }
 
-    public void showDialog(String title, String message, OnClickListener listener) {
+    public void showDialog(String title, String message, DialogInterface.OnClickListener listener) {
+        showDialog(title, message, "确定", "取消", listener, null);
+    }
+
+    public void showDialog(String title, String message, String positiveBtnText, String negativeBtnText
+            , DialogInterface.OnClickListener positiveListener, DialogInterface.OnClickListener negativeListener) {
         new AlertDialog.Builder(mContext)
                 .setTitle(title)
                 .setMessage(message)
-                .setPositiveButton("确定", listener)
-                .setNegativeButton("取消", null)
-                .create()
-                .show();
+                .setPositiveButton(positiveBtnText, positiveListener)
+                .setNegativeButton(negativeBtnText, negativeListener)
+                .create().show();
+    }
+
+    public void showOneBtnDialog(String title, String message, DialogInterface.OnClickListener listener) {
+        showOneBtnDialog(title, message, "确定", listener);
+    }
+
+    public void showOneBtnDialog(String title, String message, String btnText
+            , DialogInterface.OnClickListener listener) {
+        new AlertDialog.Builder(mContext)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(btnText, listener)
+                .create().show();
     }
 }
